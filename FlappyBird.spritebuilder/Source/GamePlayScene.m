807f2a241d2848c9c5a74b04dd117111a55c016f
ochos <H.Ochi@uea.ac.uk>
2014-10-30 11:50:08 +0000

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -11,11 +11,26 @@
     [physicsNode addChild:character];
     
     [self addObstacle];
+    timeSinceObstacle = 0.0f;
 }
 
 -(void)update:(CCTime)delta
 {
-    // put update code here
+    // this will be run every frame
+    // delta is the time that has elapsed since the last time it was run. This is usually 1/60, but can be bigger if the game slows down
+    
+    // Increment the time since the last obstracle was added
+    timeSinceObstacle += delta;  // delta is approximately 1/60th of a second
+    
+    // check to see if two seconds have passed
+    if (timeSinceObstacle > 2.0f) {
+        // add a new obstracle
+        [self addObstacle];
+        
+        //then reset the timer.
+        timeSinceObstacle = 0.0f;
+        
+    }
 }
 
 // put new methods here
