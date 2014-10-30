358480e349d63cd30010af659c3cbe6ccca95032
ochos <H.Ochi@uea.ac.uk>
2014-10-30 11:30:35 +0000

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -8,7 +8,7 @@
 {
     // your code here
     character = (Character*)[CCBReader load:@"Character"];
-    [physicsNode addChild:character];
+    //[physicsNode addChild:character];
 }
 
 -(void)update:(CCTime)delta
